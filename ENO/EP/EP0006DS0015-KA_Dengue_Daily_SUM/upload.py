ffa6060c9e26e2ce8d45b80b2591e15806d02e49
anujavenkatachalam04 <anuja.venkatachalam@ifmr.ac.in>
2024-05-15 13:51:35 +0530

    MAJOR: added code for uploading daily summaries to AWS Raw Bucket

---
+++ ENO/EP/EP0006DS0015-KA_Dengue_Daily_SUM/upload.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import re
+import os
+import boto3
+
+## -----------------------------SETTING FILE PATHS-------------------------------- ##
+
+RAW_BUCKET='dsih-artpark-01-raw-data'
+RAW_PREFIX='EPRDS8-KA_Dengue_Chikungunya_SUM/Daily/2024/By_Day/'
+client = boto3.client('s3')
+
+
+## -----------------------------UPLOAD-------------------------------------- ##
+
+# Steps:
+## 1. Download daily report excel workbook received from department via email
+## 2. Pass file path and report date (mentioned in email) into the extract_raw_summary function below 
+## which will extract the sheet and push it to the raw bucket on AWS
+
+def upload_raw_summary(raw_file_path, report_date: str) -> str:
+
+    """Extracts daily summary for specified report date from the workbook shared by GoK, and uploads to AWS S3
+
+    Raises:
+        Exception: Unable to locate raw file through raw_file_path provided
+        Exception: Unable to find sheet for report_date specified
+        Exception: Raw file already exists in AWS S3
+        Exception: Failed to upload file to AWS S3
+
+    Returns:
+        str : Success/Failure message for upload
+    """
+
+    assert re.match(r"\d{4}\-\d{2}\-\d{2}", report_date), "Invalid report date, enter as yyyy-mm-dd"
+
+    if os.path.exists(raw_file_path):
+        filename=f"wb_{report_date}.xlsx"
+        os.rename(raw_file_path, filename)
+    else:
+        raise Exception("Unable to locate raw file")
+
+    # extract date from filename
+    date_pattern=re.search(r"(\d{4}\-\d{2}\-\d{2})(.xlsx)", filename)
+    if date_pattern:
+        if date_pattern.group(1):
+            date=pd.to_datetime(date_pattern.group(1))
+            year=date.strftime("%y")
+            # assuming sheet names are in the format 4-2-24 (d-m-yy)
+            sheet_pattern=f"DDR {date.day}-{date.month}-{year}"
+        else:
+            raise Exception("Failed: Sheet not found for date.")
+    else:
+        raise Exception("Failed: Sheet not found for date.")
+
+    # open workbook, and locate sheet based on date
+    workbook=pd.ExcelFile(filename)
+    found=False
+    for sheet in workbook.sheet_names:
+        if re.search(sheet_pattern, sheet):
+            df=pd.read_excel(workbook, sheet_name=sheet)
+            RAW_FILENAME=f'{date.strftime("%Y")}-{date.strftime("%m")}-{date.strftime("%d")}.xlsx'
+            df.to_excel(f"{RAW_FILENAME}", index=False)
+            found=True
+            break
+    if not found:
+        raise Exception("Failed: Sheet not found for date.")
+
+    # boto3 upload raw to aws
+    key=f"{RAW_PREFIX}{RAW_FILENAME}"
+
+    response=client.list_objects_v2(Bucket=RAW_BUCKET,Prefix=key)
+
+    if 'Contents' in response:
+        raise Exception("Failed: File already exists in S3")
+    else:
+        try:
+            client.put_object(Bucket=RAW_BUCKET, Key=key)
+        except Exception as e:
+            return(f"Failed: Unable to upload to S3: {e}")
+    
+    os.remove(RAW_FILENAME)
+    os.remove(filename)
+    return ("Success: Raw file uploaded to S3")
+
+# sample input
+upload_raw_summary("wb_2024-05-13.xlsx")
+
