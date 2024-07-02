d1ff445ae83c950c1dba48779087c525f7d24e24
anujavenkatachalam04 <anuja.venkatachalam@ifmr.ac.in>
2024-07-02 15:46:46 +0530

    BREAKING CHANGE: changed from bash4 to bash 3 as it is the default version on mac, tagged using putobject instead of aws cp which does not support tagging anymore

--- utils/ka-dengue-upload.sh
+++ utils/ka-dengue-upload.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
 
-## INSTRUCTIONS FOR RUNNING THE SCRIPT 
-## ARGS ARE OPTIONAL:
-## FIRST ARG = Path to zip to be provided if not Downloads 
-## SECOND ARG = File/email date to be provided if not in zip folder name
+# INSTRUCTIONS FOR RUNNING THE SCRIPT 
+# ARGS ARE OPTIONAL:
+# FIRST ARG = Path to zip to be provided if not Downloads 
+# SECOND ARG = File/email date to be provided if not in zip folder name
 
-## e.g., path_to_script.sh path_to_zip_dir.sh yyyy-mm-dd 
+# e.g., path_to_script.sh path_to_zip_dir.sh yyyy-mm-dd 
 
 # Explicitly prints outputs
-set -x
+# set -x
 
 # Create a log file
-log="${HOME}/Desktop/ka-dengue-upload/ka-dengue-upload-log.log"
+log="${HOME}/Desktop/ka-dengue/upload.log"
+
+date >> "$log"
+
+rm *.xlsx
 
 # Paths to AWS S3 raw folders where data is to be uploaded
-ka_ll_path="dsih-artpark-01-raw-data/EPRDS7-KA_Dengue_Chikungunya_LL/"
-ka_sum_path="dsih-artpark-01-raw-data/EPRDS8-KA_Dengue_Chikungunya_SUM/"
+bucket="dsih-artpark-01-raw-data"
+# to be suffixed with year
+ll_prefix="EPRDS7-KA_Dengue_Chikungunya_LL"
+# to be suffixed with year
+sum_prefix="EPRDS8-KA_Dengue_Chikungunya_SUM"
 
 # If User input 1 is provided, pull directory from there, else default dir is set to Downloads
 if [ $# -gt 0 ]; then
     dir=$1
+elif ls "${HOME}/Desktop" | grep -q "ka-dengue"; then
+    dir="${HOME}/Desktop/ka-dengue/"
 else
-    dir="${HOME}/Downloads"
+    echo "Create directory ka-dengue and provide full user access. EXIT." | tee -a "$log"
+    exit 1
 fi
 
-# Find the latest added zip folder starting with "dailyreportof"
+# Find the latest added zip folder starting with "dailyreportof" (if multiple, else picks up only folder)
 latest_folder=$(ls -t "${dir}" | grep 'zip$' | grep '^dailyreportof' | head -n 1)
 
-# If no folder is found, log it
+# If no folder is found, log it and exit
 if [ -z "$latest_folder" ]; then
-    echo "$(date): No folders found." | tee -a "$log"
+    echo "$(date): No dailyreport zip folders found. EXIT." | tee -a "$log"
     exit 0
 fi
 
@@ -41,64 +51,69 @@ if [ $# -gt 1 ]; then
         current_month=$(echo $user_date | cut -d '-' -f 3)
         current_date=$(echo $user_date | cut -d '-' -f 2)
     else
-        echo "Invalid date format. Expected format: yyyy-dd-mm" | tee -a "$log"
+        echo "Invalid date format. Expected format: yyyy-dd-mm. EXIT." | tee -a "$log"
         exit 1
     fi
 else
     # Pull it from the latest folder name. If it doesn't exist, exit the code and log the error
     date_str=$(echo "$latest_folder" | grep -o '[0-9]\{8\}')
     if [ -n "$date_str" ]; then
-        current_year=${date_str:4:4}
-        current_month=${date_str:2:2}
-        current_date=${date_str:0:2}
+        current_date=$(echo "$date_str" | cut -c 1,2)
+        current_month=$(echo "$date_str" | cut -c 3,4)
+        current_year=$(echo "$date_str" | cut -b 5-8)
     else
-        echo "Date not found in folder name and user date not provided. Exiting." | tee -a "$log"
+        echo "Date not found in folder name and user date not provided. EXIT." | tee -a "$log"
         exit 1
     fi
+
+    if [ -z "$current_date" ] || [ -z "$current_month" ] || [ -z "$current_year" ]; then
+    echo "Date, month, year could not be extracted from folder name. EXIT." | tee -a "$log"
+    exit 1
+    fi
+
 fi
 
-# Main script
-# Change directory and extract the zip folder
-cd "${dir}" || { echo "Failed to change dir to ${dir}" | tee -a "$log"; exit 1; }
-tar -xf "${latest_folder}"
-extracted_dir="${latest_folder%.*}"
+tag_key="${current_year}-${current_month}-${current_date}"
 
-# Change directory to extracted folder
-cd "$extracted_dir" || { echo "Failed to change dir to ${extracted_dir}" | tee -a "$log"; exit 1; }
+# #Extract the zip folder into a folder with the same name, excl. .zip extension
+unzip -q "${dir}${latest_folder}"
 
-# Get list of files ending with .xlsx
-files=$(ls | grep '.xlsx$')
+# Create an array with the list of files in the unzipped directory
+files=()
+while IFS= read -r -d '' file; do
+    file=$(basename "$file")
+    files+=("$file")
+done < <(find . -maxdepth 1 -type f -name "*.xlsx" -print0)
 
-# Create a dictionary of expected files to keep a counter
-declare -A file_count=(["A1"]=0 ["B1"]=0 ["A2"]=0 ["B2"]=0 ["SUM"]=0)
+if [ ${#files[@]} -ne 5 ]; then
+  echo "Number of files in dir is not 5. Check for missing files/remove extra files. EXIT." | tee -a "$log"
+  exit 1
+fi
 
-# Renaming files using regex and upload them to AWS S3 with tag = date, month, year. If upload is successful, increment file count, else log error
-for file in ${files}; do
-    if echo "$file" | grep -q '^A\-1'; then
+# Rename, upload, tag for LL and summ
+for file in "${files[@]}"; do
+    if echo "$file" | grep -q '^A-1'; then
         new_name="${current_year}A1.xlsx"
-        aws s3 cp "$file" "${ka_ll_path}${current_year}/${new_name}" --tagging "Key='${current_year}-${current_month}-${current_date}', Value=''" && file_count["A1"]=1 2>> "$log"
-    elif echo "$file" | grep -q '^B\-1'; then
+        mv "$file" "$new_name" && aws s3 cp "$new_name" "s3://${bucket}/${ll_prefix}/${current_year}/${new_name}" && aws s3api put-object-tagging --bucket "${bucket}" --key "${ll_prefix}/${current_year}/${new_name}" --tagging "TagSet=[{Key=${tag_key},Value=''}]" && echo "$new_name uploaded and tagged" | tee -a "$log" 2>> "$log"
+    elif echo "$file" | grep -q '^B-1'; then
         new_name="${current_year}B1.xlsx"
-        aws s3 cp "$file" "${ka_ll_path}${current_year}/${new_name}" --tagging "Key='${current_year}-${current_month}-${current_date}', Value=''" && file_count["B1"]=1 2>> "$log"
-    elif echo "$file" | grep -q '^A\-2'; then
+        mv "$file" "$new_name" && aws s3 cp "$new_name" "s3://${bucket}/${ll_prefix}/${current_year}/${new_name}" && aws s3api put-object-tagging --bucket "${bucket}" --key "${ll_prefix}/${current_year}/${new_name}" --tagging "TagSet=[{Key=${tag_key},Value=''}]" && echo "$new_name uploaded and tagged" | tee -a "$log" 2>> "$log"
+    elif echo "$file" | grep -q '^A-2'; then
         new_name="${current_year}A2.xlsx"
-        aws s3 cp "$file" "${ka_ll_path}${current_year}/${new_name}" --tagging "Key='${current_year}-${current_month}-${current_date}', Value=''" && file_count["A2"]=1 2>> "$log"
-    elif echo "$file" | grep -q '^B\-2'; then
+        mv "$file" "$new_name" && aws s3 cp "$new_name" "s3://${bucket}/${ll_prefix}/${current_year}/${new_name}" && aws s3api put-object-tagging --bucket "${bucket}" --key "${ll_prefix}/${current_year}/${new_name}" --tagging "TagSet=[{Key=${tag_key},Value=''}]" && echo "$new_name uploaded and tagged" | tee -a "$log" 2>> "$log"
+    elif echo "$file" | grep -q '^B-2'; then
         new_name="${current_year}B2.xlsx"
-        aws s3 cp "$file" "${ka_ll_path}${current_year}/${new_name}" --tagging "Key='${current_year}-${current_month}-${current_date}', Value=''" && file_count["B2"]=1 2>> "$log"
-    else
+        mv "$file" "$new_name" && aws s3 cp "$new_name" "s3://${bucket}/${ll_prefix}/${current_year}/${new_name}" && aws s3api put-object-tagging --bucket "${bucket}" --key "${ll_prefix}/${current_year}/${new_name}" --tagging "TagSet=[{Key=${tag_key},Value=''}]" && echo "$new_name uploaded and tagged" | tee -a "$log" 2>> "$log"
+    elif echo "$file" | grep -q '^[0-9]'; then
         new_name="${current_year}-${current_month}.xlsx"
-        aws s3 cp "$file" "${ka_sum_path}${current_year}/${new_name}" --tagging "Key='${current_year}-${current_month}-${current_date}', Value=''" && file_count["SUM"]=1 2>> "$log"
+        mv "$file" "$new_name" && aws s3 cp "$new_name" "s3://${bucket}/${sum_prefix}/${current_year}/${new_name}" && aws s3api put-object-tagging --bucket "${bucket}" --key "${sum_prefix}/${current_year}/${new_name}" --tagging "TagSet=[{Key=${tag_key},Value=''}]" && echo "$new_name uploaded and tagged" | tee -a "$log" 2>> "$log"
+    else 
+        echo "Skipping file '$file' - does not match any pattern - start with A-1, B-1, A-2, B-2 or in format 06 - June 2024 F24. Correct format to startin" | tee -a "$error_log"
+        continue
     fi
-    mv "$file" "$new_name"
 done
 
-# If file counts are not equal to 1, print in stdout and append to log file
-if [ ${file_count["A1"]} -eq 1 ] && [ ${file_count["B1"]} -eq 1 ] && [ ${file_count["A2"]} -eq 1 ] && [ ${file_count["B2"]} -eq 1 ] && [ ${file_count["SUM"]} -eq 1 ]; then
-    echo "Folder ${extracted_dir}: LL & SUMM uploaded" 2>&1 | tee -a "$log"
-    cd ..
-    rm -rf "$extracted_dir"
-    rm -rf "$latest_folder"
-else
-    echo "Folder ${extracted_dir}: Missing files in upload." 2>&1 | tee -a "$log"
-fi
+rm *.zip
+rm *.xlsx
+
+
