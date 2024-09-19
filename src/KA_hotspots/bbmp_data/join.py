c2ce03d275eab8ecb36089f9ad37371ca7080b16
anujavenkatachalam04 <anuja.venkatachalam@ifmr.ac.in>
2024-09-20 00:20:26 +0530

    reorg hotspots

--- src/KA_hotspots/bbmp_data/join.py
+++ src/KA_hotspots/bbmp_data/join.py
@@ -1,3 +1,5 @@
+# get data from dsih-artpark-01-raw-data/EPRDS2-Bengaluru_City_Dengue_LL/consolidated/
+
 import pandas as pd
 
 import os
@@ -9,9 +11,15 @@ for file in os.listdir("bbmp_data"):
 
         main_df=main_df._append(df)
 
+print(main_df.columns)
 
-df1=pd.read_csv("bbmp_data/bbmp_24to11.csv")
+df1=pd.read_csv("bbmp_data/bbmp_aug24tosep16.csv")
+
+print(df1.columns)
+
+print("Check col names before appending")
 
 main_df=df1._append(main_df)
 
-main_df.to_csv("bbmp_data/bbmp_24to13.csv", index=False)
+# change manually
+main_df.to_csv("bbmp_data/bbmp_aug24tosep17.csv", index=False)
